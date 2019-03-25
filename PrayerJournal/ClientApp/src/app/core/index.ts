9087a3f621445c29d21241b4419b0509b802b42e
Brady Marston <bradymarston@live.com>
2019-03-25 09:34:06 -0500

    Removed test files

--- PrayerJournal/ClientApp/src/app/core/index.ts
+++ PrayerJournal/ClientApp/src/app/core/index.ts
@@ -1,6 +1,5 @@
 export * from './core.module';
 export * from './authentication/authentication.service';
-export * from './authentication/authentication.service.mock';
 export * from './authentication/authentication.guard';
 export * from './authorization.service';
 export * from './i18n.service';
