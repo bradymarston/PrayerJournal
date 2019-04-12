d815b9a48c2dd19527b673113fdf0bc8dbda2e08
Brady Marston <bradymarston@live.com>
2019-04-12 17:43:02 -0500

    Removed quotes from home page

--- PrayerJournal/ClientApp/src/app/home/home.component.ts
+++ PrayerJournal/ClientApp/src/app/home/home.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { finalize } from 'rxjs/operators';
 
-import { QuoteService } from './quote.service';
 import { ActivatedRoute } from '@angular/router';
 import { UserAdminService } from '../core';
 
@@ -11,23 +9,13 @@ import { UserAdminService } from '../core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
-  quote: string;
-  isLoading: boolean;
-
-  constructor(private quoteService: QuoteService, private route: ActivatedRoute, private userAdminService: UserAdminService) { }
+  constructor(private route: ActivatedRoute, private userAdminService: UserAdminService) { }
 
   ngOnInit() {
     this.route.queryParamMap.subscribe((params) => {
-      console.log(params);
       if (params.get("refreshRoles"))
         this.refreshRoles();
     });
-
-    this.isLoading = true;
-    this.quoteService.getRandomQuote({ category: 'dev' })
-      .pipe(finalize(() => { this.isLoading = false; }))
-      .subscribe((quote: string) => { this.quote = quote; });
   }
 
   refreshRoles() {
